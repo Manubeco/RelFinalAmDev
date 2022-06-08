86c0262ec0d604de6cc3e79de3278473833418ef
Manubeco <emanuelbeco@outlook.com>
2022-06-08 12:50:43 -0300

    criacao da casca da classe Calculadora

---
+++ RelFinalAmbDev/src/Calculadora.java
@@ -0,0 +1,29 @@
+import java.lang.Math;
+
+public class Calculadora {
+    
+    public void soma(){
+    }
+    
+    public void subtracao(){
+    }
+    
+    public void multiplicacao(){
+    }
+    
+    public void divisao(){
+    }
+    
+    public void exponencial(){
+    }
+    
+    public void raizQuadrada(){
+    }
+    
+    public void teto(){
+    }
+    
+    public void piso(){
+    }
+    
+}
