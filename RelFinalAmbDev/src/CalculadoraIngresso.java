7ca0da7305878fffeb341bde8ef88c8ee377f609
marcelogomesdc <adsmarcelogomes@gmail.com>
2022-06-08 21:45:28 -0300

    Inserindo classes de adição e subtração

---
+++ RelFinalAmbDev/src/CalculadoraIngresso.java
@@ -0,0 +1,9 @@
+public class VendaIngressos {
+    public static void main(String args[]) {
+      int quantidadeTotal = 100;
+      int ingressosVendidos = 30;
+      int quantidadeRestante = quantidadeTotal - ingressosVendidos;
+      
+      System.out.println("A quantidade de ingressos restantes é: " + quantidadeRestante);
+    }
+}
---
+++ RelFinalAmbDev/src/CalculadoraLivraria.java
@@ -0,0 +1,10 @@
+public class VendaLivros {
+    public static void main(String args[]) {
+      int livro1 = 70;
+      int livro2 = 60;
+      
+      int total= livro1 + livro2;
+
+      System.out.println("A valor total do preço dos dois livros é: " + total);
+    }
+}
