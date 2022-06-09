7c89bca639f2d761257d29e75dfecc2cdcb2889c
Morgana Ramos <97921717+MorganaRamos@users.noreply.github.com>
2022-06-09 09:21:04 -0300

    Classe Imc

---
+++ RelFinalAmbDev/src/Imc.java
@@ -0,0 +1,18 @@
+    import util. Calculadora;
+
+public class Imc {
+
+   double peso = 100;
+   float altura = 1.80;
+   double =  multAltura;
+   double = imc;
+
+   public static void main(String[] args) {
+      
+       multAltura = Calculadora.multiplicacao(altura, altura);  
+       imc = peso / multAltura;
+      
+      System.out.println(imc);
+   }
+}
+
